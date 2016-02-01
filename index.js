94c327f00dca6d56251d07c421acd179335a9a22
alvinchengyi <alvin.cheng@yale.edu>
2016-02-01 14:20:59 +0000

    pesist user to MongoDB

--- index.js
+++ index.js
@@ -2,38 +2,66 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 var app = express();
 var bodyParser = require('body-parser');
-var Users = require('./models/users.js')
+var session = require('express-session');
+var Users = require('./models/users.js');
 
 //Configure our app
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: {secure: 'auto'}
+}))
+
+app.use(function(req, res, next){
+  if(req.session.userId){
+    Users.findById(req.session.userId, function(err, user){
+      if(!err){
+        res.locals.currentUser = user;
+      }
+      next();
+    })
+  }else{
+    next();
+  }
+})
+
 
 app.get('/', function (req, res) {
-  Users.find({}, function (err, users) {
+  Users.count(function (err, users) {
     if (err) {
       res.send('error getting users');
     }else{
-      res.render('index', {userCount: users.length});
+      res.render('index', {
+        userCount: users.length,
+        currentUser: res.locals.currentUser
+      });
     }
   });
 });
     
 app.post('/user/register', function (req, res) {
+  if(req.body.password !== req.body.password_confirmation){
+    return res.render('index', {errors: "Password and password confirmation do not match"});
+  }
+
   var newUser = new Users();
   newUser.hashed_password = req.body.password;
   newUser.email = req.body.email;
   newUser.name = req.body.fl_name;
-  newUser.save(function(err){
+  newUser.save(function(err, user){
+   req.session.userId  = user._id;
     if(err){
-      res.send('there was an error saving the user');
+      res.render('index', {errors: err});
     }else{
       res.redirect('/');
     }
   })
-    res.send(req.body);
-    console.log('The user has the emial address', req.body.email);
+  console.log('The user has the emial address', req.body.email);
 });
 
 app.listen(process.env.PORT, function () {
